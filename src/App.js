89c8377b9da9254c1288a6bdc7717a4b6b64572a
Anushka Chauhan <anushkachauhan1503@gmail.com>
2022-12-15 11:21:14 +0530

    Router + Pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import React from "react";
+import { Link, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Articles from "./pages/Articles";
 
 const App = () => {
-  return <div className="app">App</div>;
+  return (
+    <div className="app">
+      <h1>SSR Example</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/articles">Articles</Link>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/articles" element={<Articles />} />
+        </Routes>
+      </main>
+    </div>
+  );
 };
 
 export default App;
